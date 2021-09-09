b49719815b5237c3d9801ac3ccd450fab11dcfa3
William Hutt <whutt1994@gmail.com>
2021-09-09 13:03:05 -0700

    feat: adds makeStyles support

--- src/sidebar/sidebar.tsx
+++ src/sidebar/sidebar.tsx
@@ -2,16 +2,36 @@ import React from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import styles from '../../styles/components/sidebar.module.css'
+// import styles from '../../styles/components/sidebar.module.css'
+import { makeStyles } from '@material-ui/core/styles';
 
 // use list
+// thoughts for Routing - nav items ticket #11
 // look into how to use with import { useRouter } from 'next/router';
 // use the adapter versions?
 // I.E. NextLinkComposed and Link
 
+const useStyles = makeStyles({
+    nested: {
+        paddingLeft: '1.5rem',
+        fontSize: '0.875rem',
+        paddingTop: '0rem',
+        paddingBottom: '0rem',
+    },
+    container: {
+        width: '25%',
+    },
+    subNavHeading: {
+        color: '#95A1AD',
+    }
+})
+
+// primary={<Typography className={classes.nested}>Books</Typography>}
+
 export const Sidebar: React.FC = () => {
+    const classes = useStyles()
     return (
-        <List component="nav" className={styles.container}>
+        <List component="nav" className={classes.container}>
             <ListItem>
                 <ListItemText primary="Communities" />
             </ListItem>
@@ -22,40 +42,40 @@ export const Sidebar: React.FC = () => {
                 <ListItemText primary="Events" />
             </ListItem>
             <ListItem>
-                <ListItemText primary="Education" />
+                <ListItemText primary="Education" className={classes.subNavHeading} />
             </ListItem>
-            <List component="div">
+            <List component="div" className={classes.nested}>
                 <ListItem>
-                    <ListItemText primary="Books" className={styles.nested} />
+                    <ListItemText disableTypography primary="Books" />
                 </ListItem>
                 <ListItem>
-                    <ListItemText primary="Tutorials" className={styles.nested} />
+                    <ListItemText disableTypography primary="Tutorials" />
                 </ListItem>
                 <ListItem>
-                    <ListItemText primary="Courses" className={styles.nested} />
+                    <ListItemText disableTypography primary="Courses" />
                 </ListItem>
                 <ListItem>
-                    <ListItemText primary="Workshops" className={styles.nested} />
+                    <ListItemText disableTypography primary="Workshops" />
                 </ListItem>
                 <ListItem>
-                    <ListItemText primary="Trainers" className={styles.nested} />
+                    <ListItemText disableTypography primary="Trainers" />
                 </ListItem>
                 <ListItem>
-                    <ListItemText primary="Schools" className={styles.nested} />
+                    <ListItemText disableTypography primary="Schools" />
                 </ListItem>
             </List>
             <ListItem>
-                <ListItemText primary="Tools" />
+                <ListItemText primary="Tools" className={classes.subNavHeading} />
             </ListItem>
-            <List component="div">
+            <List component="div" className={classes.nested}>
                 <ListItem>
-                    <ListItemText primary="IDEs" className={styles.nested} />
+                    <ListItemText disableTypography primary="IDEs" />
                 </ListItem>
                 <ListItem>
-                    <ListItemText primary="Frameworks" className={styles.nested} />
+                    <ListItemText disableTypography primary="Frameworks" />
                 </ListItem>
                 <ListItem>
-                    <ListItemText primary="Libraries" className={styles.nested} />
+                    <ListItemText disableTypography primary="Libraries" />
                 </ListItem>
             </List>
             <hr/>
---
+++ src/theme.tsx
@@ -0,0 +1,22 @@
+import { createTheme } from '@material-ui/core/styles';
+import red from '@material-ui/core/colors/red';
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+});
+
+export default theme;
