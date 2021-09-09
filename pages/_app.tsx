0f1d48068ac2f3a10a8c97dc4a2fafd76ef58347
morganistic <morgan@border.dev>
2021-09-09 14:03:49 -0600

    added localization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
 import React, { ReactElement } from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { IntlErrorCode, NextIntlProvider } from 'next-intl';
+
+function onError(error) {
+  if (process.env.NODE_ENV !== 'production') {
+    if (error.code === IntlErrorCode.MISSING_MESSAGE) {
+      console.warn(error);
+    } else {
+      console.error(error);
+    }
+  }
+}
+
+function getMessageFallback({ namespace, key, error }) {
+  const path = [namespace, key].filter((part) => part != null).join('.');
+
+  if (error.code === IntlErrorCode.MISSING_MESSAGE) {
+    return `${path} is not yet translated`;
+  }
+  return `Fix translation message at: ${path}`;
+}
 
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
-  return <Component {...pageProps} />;
+  return (
+    <NextIntlProvider messages={pageProps.messages} onError={onError} getMessageFallback={getMessageFallback}>
+      <Component {...pageProps} />
+    </NextIntlProvider>
+  );
 }
 export default MyApp;
