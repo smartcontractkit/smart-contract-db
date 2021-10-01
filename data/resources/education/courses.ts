b2d402bf2375855cff44048bec95c5ec2ae7e6b1
morganistic <morgan@border.dev>
2021-09-30 22:35:52 -0600

    added to database

--- data/resources/education/courses.ts
+++ data/resources/education/courses.ts
@@ -3,6 +3,7 @@ export interface Course {
   title: string;
   description: string;
   link: string;
+  src?: string;
 }
 
 export const courses: Course[] = [
@@ -11,17 +12,27 @@ export const courses: Course[] = [
     title: 'Blockchain Specialization',
     description: '',
     link: 'https://www.coursera.org/specializations/blockchain',
+    src: '',
   },
   {
     id: 'ethereum-and-solidity-the-complete-developers-guide',
     title: "Ethereum and Solidity: The Complete Developer's Guide",
     description: 'Use Ethereum, Solidity, and Smart Contracts to build production-ready apps based on the blockchain',
     link: 'https://www.udemy.com/course/ethereum-and-solidity-the-complete-developers-guide/',
+    src: '',
   },
   {
     id: 'getting-started-with-ethereum-solidity-development',
     title: 'Become a Blockchain Developer with Ethereum and Solidity',
     description: '',
     link: 'https://www.udemy.com/course/getting-started-with-ethereum-solidity-development/',
+    src: '',
+  },
+  {
+    id: 'crypto-zombies',
+    title: 'Crypto Zombies',
+    description: '',
+    link: 'https://cryptozombies.io/',
+    src: '',
   },
 ];
