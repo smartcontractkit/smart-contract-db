a95d57b77fc2cf40c763069b0ea68a2cb042b1b5
William Hutt <whutt1994@gmail.com>
2021-10-11 10:22:47 -0700

    fix: resolves style hover issue & testing drawer perf

--- src/header/hamburgerMenu.tsx
+++ src/header/hamburgerMenu.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Sidebar } from 'src/sidebar';
+import { Drawer, IconButton } from '@material-ui/core';
 import { Icon } from '../shared/icon';
 import styles from './hamburgerMenu.module.css';
 
@@ -26,32 +27,60 @@ export const HamburgerMenu: React.FC = () => {
   return (
     <>
       {/* change opacity to be 0.3 on openDrawer */}
-      {/* <Drawer anchor="left" className={styles.drawer} onClose={() => setOpenDrawer(false)} open={openDrawer}> */}
-      <div className={styles.drawer} role="presentation">
+      <Drawer anchor="left" className={styles.drawer} onClose={() => setOpenDrawer(false)} open={openDrawer}>
         {showClose && (
-          <>
-            <div className={styles.backdrop} />
-            <div role="presentation" className={styles.closeContainer} onClick={() => setOpenDrawer(false)}>
-              <p className={styles.close}>Close</p>
-              <Icon className={styles.closeIconToggle} name="close" size={32} />
-            </div>
-            <div className={styles.sidebar}>
-              <div role="presentation" onClick={() => setOpenDrawer(false)} className={styles.hamburgerMenu}>
-                <Sidebar />
-              </div>
-            </div>
-          </>
+          <div role="presentation" className={styles.closeContainer} onClick={() => setOpenDrawer(false)}>
+            <p className={styles.close}>Close</p> <Icon className={styles.closeIconToggle} name="close" size={32} />
+          </div>
         )}
-      </div>
-      {/* </Drawer> */}
+
+        <div role="presentation" onClick={() => setOpenDrawer(false)} className={styles.hamburgerMenu}>
+          <Sidebar />
+        </div>
+      </Drawer>
       <div className={openDrawer ? styles.opacityHamburger : styles.hamburgerContainer}>
         <div className={styles.browse}>Browse</div>
-        <button type="button" className={styles.iconButtonContainer} onClick={() => setOpenDrawer(!openDrawer)}>
-          <span className={styles.menuLabel}>
-            <Icon className={styles.menuIconToggle} name="hamburger" size={48} />
-          </span>
-        </button>
+        <IconButton className={styles.iconButtonContainer} onClick={() => setOpenDrawer(!openDrawer)} disableRipple>
+          <Icon className={styles.menuIconToggle} name="hamburger" size={48} />
+        </IconButton>
       </div>
     </>
   );
+
+  // return (
+  //   <>
+  //     {/* change opacity to be 0.3 on openDrawer */}
+  //     {/* <Drawer anchor="left" className={styles.drawer} onClose={() => setOpenDrawer(false)} open={openDrawer}> */}
+  //     <div className={styles.drawer} role="presentation">
+  //       {showClose && (
+  //         <>
+  //           <div className={styles.backdrop} />
+  //           <div role="presentation" className={styles.closeContainer} onClick={() => setOpenDrawer(false)}>
+  //             <p className={styles.close}>Close</p>
+  //             <Icon className={styles.closeIconToggle} name="close" size={32} />
+  //           </div>
+  //           <div className={styles.sidebar}>
+  //             <div role="presentation" onClick={() => setOpenDrawer(false)} className={styles.hamburgerMenu}>
+  //               <Sidebar />
+  //             </div>
+  //           </div>
+  //         </>
+  //       )}
+  //     </div>
+  //     {/* </Drawer> */}
+  //     <div className={openDrawer ? styles.opacityHamburger : styles.hamburgerContainer}>
+  //       <div className={styles.browse}>Browse</div>
+  //       <button
+  //         type="button"
+  //         aria-label="Mobile Menu"
+  //         className={styles.iconButtonContainer}
+  //         onClick={() => setOpenDrawer(!openDrawer)}
+  //       >
+  //         <span className={styles.menuLabel}>
+  //           <Icon className={styles.menuIconToggle} name="hamburger" size={48} />
+  //         </span>
+  //       </button>
+  //     </div>
+  //   </>
+  // );
 };
