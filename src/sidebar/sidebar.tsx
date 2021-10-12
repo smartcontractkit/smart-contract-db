69dfc150d6168a58823ac79c40680b2fa5024d4c
William Hutt <whutt1994@gmail.com>
2021-10-12 15:35:07 -0700

    feat: adds wallets page (#157)

--- src/sidebar/sidebar.tsx
+++ src/sidebar/sidebar.tsx
@@ -18,6 +18,7 @@ const toolsNav = [
   { title: 'Libraries', path: '/libraries' },
   { title: 'Exchanges', path: '/exchanges' },
   { title: 'Languages', path: '/languages' },
+  { title: 'Wallets', path: '/wallets' },
   { title: 'Deploy', path: '/deploy' },
   { title: 'Administration', path: '/administration' },
   { title: 'Security', path: '/security' },
