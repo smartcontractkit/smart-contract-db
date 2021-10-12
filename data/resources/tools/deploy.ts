2e73ebc7bde280550895e2f1b64fa52f961bb64c
William Hutt <whutt1994@gmail.com>
2021-10-12 12:00:44 -0700

    feat: adds deploy page

---
+++ data/resources/tools/deploy.ts
@@ -0,0 +1,88 @@
+export interface Deploy {
+  id: string;
+  title: string;
+  description: string;
+  link: string;
+  src?: string;
+}
+
+export const deploy: Deploy[] = [
+  {
+    id: 'etherscan',
+    title: 'Etherscan',
+    description:
+      'Etherscan allows you to explore and search the Ethereum blockchain for transactions, addresses, tokens, prices and other activities taking place on Ethereum (ETH)',
+    link: 'https://etherscan.io/',
+    src: '/img/deploy/etherscan.jpg',
+  },
+  {
+    id: 'open zeppelin supernode',
+    title: 'Open Zeppelin Supernode',
+    description: '',
+    link: '',
+    src: '',
+  },
+  {
+    id: 'infura',
+    title: 'Infura',
+    description: '',
+    link: '',
+    src: '',
+  },
+  {
+    id: 'chainstack',
+    title: 'Chainstack',
+    description: '',
+    link: '',
+    src: '',
+  },
+  {
+    id: 'quicknode',
+    title: 'Quicknode',
+    description: '',
+    link: '',
+    src: '',
+  },
+  {
+    id: 'linkPool naaS',
+    title: 'LinkPool NaaS',
+    description: '',
+    link: '',
+    src: '',
+  },
+  {
+    id: 'openSea',
+    title: 'OpenSea',
+    description: '',
+    link: '',
+    src: '',
+  },
+  {
+    id: 'truffle',
+    title: 'Truffle',
+    description: '',
+    link: '',
+    src: '',
+  },
+  {
+    id: 'hardHat',
+    title: 'HardHat',
+    description: '',
+    link: '',
+    src: '',
+  },
+  {
+    id: 'truffle',
+    title: 'Truffle',
+    description: '',
+    link: '',
+    src: '',
+  },
+  {
+    id: 'dapp tools',
+    title: 'Dapp Tools',
+    description: '',
+    link: '',
+    src: '',
+  },
+];
