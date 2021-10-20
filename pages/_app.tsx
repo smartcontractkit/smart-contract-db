dba66d6fe36157f34f8c110c8e4f51c071f5785f
William Hutt <whutt1994@gmail.com>
2021-10-20 12:05:58 -0700

    wip

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -55,9 +55,19 @@ function MyApp({ Component, pageProps }: AppProps): ReactElement {
       <Head>
         <title>My page</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        {/* <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-   })(window,document,'script','dataLayer','GTM-5J5DL65');</script> */}
+        {/* GTM-5J5DL65 */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+        <script
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+          `,
+          }}
+        />
       </Head>
 
       <NextIntlProvider messages={pageProps.messages} onError={onError} getMessageFallback={getMessageFallback}>
