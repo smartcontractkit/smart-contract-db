5a0b744d7a185e0808489c69b472dfb0f4ccf473
William Hutt <whutt1994@gmail.com>
2021-10-21 14:49:17 -0700

    feat: adds sub sections for communities

--- src/content/communities.tsx
+++ src/content/communities.tsx
@@ -1,13 +1,38 @@
 import React from 'react';
 import { CategoryList } from 'src/shared/category-list';
 import styles from './content.module.css';
-import { communities } from '@/data/resources/communities/communities';
+// import { communities } from '@/data/resources/communities/communities';
+import { ventureCapitalFunds } from '@/data/resources/communities/venture-capital-funds';
+import { grantPrograms } from '@/data/resources/communities/grant-programs';
+import { incubatorsAccelerators } from '@/data/resources/communities/incubators-accelerators';
+import { developer } from '@/data/resources/communities/developer';
+import { enterprise } from '@/data/resources/communities/enterprise';
 
 export const Communities: React.FC = () => {
+  const communities = [
+    ...ventureCapitalFunds,
+    ...grantPrograms,
+    ...incubatorsAccelerators,
+    ...developer,
+    ...enterprise,
+  ];
+  // console.log(communities);
   return (
     <section className={styles.section}>
-      <div className={styles.heading}>Communities</div>
-      <CategoryList data={communities} />
+      <div className={styles.communitiesHeading}>Communities</div>
+      <CategoryList name="communities" data={communities} />
+
+      {/* Venture Capital Funds */}
+      {/* <li className={styles.hr} /> */}
+      {/* <CategoryList data={ventureCapitalFunds} /> */}
+      {/* Grant Programs */}
+      {/* <CategoryList data={grantPrograms} /> */}
+      {/* Incubators/Accelerators */}
+      {/* <CategoryList data={incubatorsAccelerators} /> */}
+      {/* Developer */}
+      {/* <CategoryList data={developer} /> */}
+      {/* Enterprise */}
+      {/* <CategoryList data={enterprise} /> */}
     </section>
   );
 };
--- src/shared/category-list/category-list.tsx
+++ src/shared/category-list/category-list.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable no-nested-ternary */
 import React from 'react';
+import { isDatePast } from 'src/content/events';
 import styles from './category-list.module.css';
 import { Icon } from '../icon';
 import { Initicon } from '../initicon';
 import Link from '../../link';
-import { isDatePast } from 'src/content/events';
 
 export interface CategoryListProps {
   name?: string; // category's name
@@ -32,10 +33,60 @@ function dateFormatter(date: string): string {
 export const CategoryList: React.FC<CategoryListProps> = ({ name, data }) => {
   const identiconSeedMax = (max: number) => Math.floor(Math.random() * max);
   const dataWithOngoingDates = data.filter(({ startDate, endDate }) => !isDatePast(startDate, endDate));
-
+  // console.log(typeof Object.values(data[0])[0].length);
   return (
     <div className={styles.container}>
-      {dataWithOngoingDates.length ? (
+      {/* for communities */}
+      {name === 'communities' ? (
+        <ul className={styles.ul}>
+          {data.map((communityItem, index) => (
+            <React.Fragment key={Object.keys(communityItem) as unknown as string}>
+              {index === 0 ? (
+                <>
+                  <div className={styles.communitySubHeaders}>{Object.keys(communityItem)}</div>{' '}
+                  <li className={styles.hr} />
+                </>
+              ) : (
+                ''
+              )}
+              {Object.values(communityItem)[0].map(
+                ({ id, title, src, startDate, description, link }, communityItemIndex) => (
+                  <>
+                    <li key={id} className={styles.listItem}>
+                      <Link className={styles.link} rel="noopener noreferrer" href={link}>
+                        <div className={styles.avatarContainer}>
+                          {src ? (
+                            <div className={styles.avatar}>
+                              <img alt={title} src={src} width="88" height="" />
+                            </div>
+                          ) : (
+                            <Initicon size={88} text={title} seed={identiconSeedMax(9)} single={false} />
+                          )}
+                        </div>
+                        <div>
+                          <div className={styles.title}>
+                            <Icon className={styles.listItemArrowIcon} name="long-arrow-up" size={12} />
+                            <span className={styles.listItemTitle}>{title}</span>
+                          </div>
+                          {startDate ? <div className={styles.subTitle}>{dateFormatter(startDate)}</div> : ''}
+                          <div className={styles.listItemText}>{description}</div>
+                        </div>
+                      </Link>
+                    </li>
+                    {communityItem.length === communityItemIndex + 1 ? (
+                      <div className={styles.communitySubHeaders}>{Object.keys(communityItem)}</div>
+                    ) : (
+                      ''
+                    )}
+                    {communityItem.length !== communityItemIndex + 1 ? <li className={styles.hr} /> : ''}
+                  </>
+                )
+              )}
+              {/* {data.length !== index + 1 ? <li className={styles.hr} /> : ''} */}
+            </React.Fragment>
+          ))}
+        </ul>
+      ) : dataWithOngoingDates.length && name !== 'communities' ? (
         <ul className={styles.ul}>
           {data.map(({ id, title, src, startDate, description, link }, index: number) => (
             <React.Fragment key={id}>
