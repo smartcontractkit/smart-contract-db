4ca43abdf0ca58ebbabe147b26e2d13ff4428ae0
William Hutt <whutt1994@gmail.com>
2021-10-22 12:15:24 -0700

    refactor: swaps to hardcoded value

--- lib/ga/index.tsx
+++ lib/ga/index.tsx
@@ -1,6 +1,6 @@
 // log the pageview with their URL
 export const pageView = (url: string): void => {
-  window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+  window.gtag('config', 'GTM-5J5DL65', {
     page_path: url,
   });
 };
