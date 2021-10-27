4b33ae64586494d41857d6c9fbc95b25a0a801b2
William Hutt <whutt1994@gmail.com>
2021-10-27 09:37:48 -0700

    fix: changes base name

--- .github/actions/resource-links/index.js
+++ .github/actions/resource-links/index.js
@@ -2,17 +2,27 @@
 const fs = require('fs');
 const core = require('@actions/core');
 
+// function communityValidation() {
+//     communities.map((communityItem) => (
+  
+//           Object.values(communityItem)[0]
+//             .map(({ id, title, src, startDate, description, link }, communityItemIndex) => (
+//                 console.log(link)
+//             ))
+//       ))
+// }
+
+// function validation() {
+//     data.map(({ id, title, src, startDate, description, link }, index) => (
+//         console.log(link)
+//       ))
+// }
+
 async function validateResourceLinks(){
 
     core.info('Hello Will!!!')
+    const data = fs.writeFileSync('./hello.txt', 'YO')
 
-    // communities.map((communityItem) => (
-  
-    //       Object.values(communityItem)[0]
-    //         .map(({ id, title, src, startDate, description, link }, communityItemIndex) => (
-    //             console.log(link)
-    //         ))
-    //   ))
   
   }
   
