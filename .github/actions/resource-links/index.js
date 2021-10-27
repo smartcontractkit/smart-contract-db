2d897dba453e304af81c1e8bc2e024809f8bfc40
William Hutt <whutt1994@gmail.com>
2021-10-27 09:27:05 -0700

    fix: adds deps

--- .github/actions/resource-links/index.js
+++ .github/actions/resource-links/index.js
@@ -5,6 +5,14 @@ const core = require('@actions/core');
 async function validateResourceLinks(){
 
     core.info('Hello Will!!!')
+
+    // communities.map((communityItem) => (
+  
+    //       Object.values(communityItem)[0]
+    //         .map(({ id, title, src, startDate, description, link }, communityItemIndex) => (
+    //             console.log(link)
+    //         ))
+    //   ))
   
   }
   
