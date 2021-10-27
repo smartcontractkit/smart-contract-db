c9efca532e5d00f59ef4776e8400e9592d8d8dc0
William Hutt <whutt1994@gmail.com>
2021-10-27 09:18:18 -0700

    feat: testing github action

---
+++ .github/actions/resource-links/index.js
@@ -0,0 +1,11 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+const core = require('@actions/core');
+
+async function validateResourceLinks(){
+
+    core.info('Hello Will!!!')
+  
+  }
+  
+  validateResourceLinks();
