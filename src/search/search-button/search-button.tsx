de70b0abc0664bd9d0efa41973f4112eef5a28b2
morganistic <morgan@border.dev>
2021-10-28 12:27:24 -0600

    header opacity fix

--- src/search/search-button/search-button.tsx
+++ src/search/search-button/search-button.tsx
@@ -1,3 +1,4 @@
+import { useMediaQuery } from '@/hooks/media-query';
 import * as React from 'react';
 import { Icon } from 'src/shared/icon';
 import styles from './search-button.module.css';
@@ -6,10 +7,22 @@ type SearchButtonProps = {
   onClick: () => void;
 };
 
-export const SearchButton: React.FC<SearchButtonProps> = ({ onClick }) => (
-  <button onClick={onClick} className={styles.button}>
-    <Icon className={styles.searchIcon} name="search" size={16} color="#95A1AD" />
-    <span className={styles.text}>Search</span>
-    <Icon className={styles.subtractIcon} name="subtract" size={15} color="#78838F" />
-  </button>
-);
+export const SearchButton: React.FC<SearchButtonProps> = ({ onClick }) => {
+  const isMobile = useMediaQuery(600);
+
+  return (
+    <>
+      {!isMobile ? (
+        <button onClick={onClick} className={styles.desktop_button}>
+          <Icon className={styles.search_icon} name="search" size={16} color="#95A1AD" />
+          <span className={styles.text}>Search</span>
+          <Icon className={styles.subtract_icon} name="subtract" size={15} color="#78838F" />
+        </button>
+      ) : (
+        <button onClick={onClick} className={styles.mobile_button}>
+          <Icon className={styles.search_icon} name="search" size={25} color="#25292E" />
+        </button>
+      )}
+    </>
+  );
+};
