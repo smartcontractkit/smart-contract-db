7676e8dc6b0e668abe85b08af1ba2089eb4776c0
William Hutt <whutt1994@gmail.com>
2021-10-29 12:53:50 -0700

    fix: storybook issue

--- src/shared/category-list/category-list.stories.tsx
+++ src/shared/category-list/category-list.stories.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react';
 import { CategoryList } from './category-list';
-import { books } from '@/data/resources/education/books';
+import * as books from '@/data/resources/education/books.json';
 
 export default {
   title: 'CategoryList',
   component: CategoryList,
 } as Meta;
 
-const Template: Story = () => <CategoryList data={books} />;
+const Template: Story = () => <CategoryList data={books[0]} />;
 
 export const Default = Template.bind({});
